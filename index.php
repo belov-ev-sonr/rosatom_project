ef2810a25aa737c8bbf18ebeb3fa124c84936f5b
RedO17th <saratovcev-1995@mail.ru>
2020-09-12 00:07:08 +0300

    init setting slim framework

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-phpinfo();
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/vendor/autoload.php';
+
+$app = new App();
+
+$app->get('/help', function (Request $request, Response $response, $args) {
+    return $response->getBody()->write('hello');
+});
+
+$app->run();
